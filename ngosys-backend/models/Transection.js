9c3becdfd59d68c9522b29ecc76497b06f58ad3b
harikapadia999 <harikapadia999@gmail.com>
2023-11-22 19:38:09 +0530

    Update Transection.js

--- ngosys-backend/models/Transection.js
+++ ngosys-backend/models/Transection.js
@@ -1,30 +1,36 @@
 import mongoose from "mongoose";
 
 const transectionSchema = new mongoose.Schema({
-    ngoid: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'NGO',
-        required: true,
-    },
-    donorid: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Donor',
-        required: true,
-    },
-    amount:{
-        type: Number,
-        required: true,
-    },
-    date:{
-        type: Date,
-        default: Date.now,
-        required: true,
-    },
-    razorpayPaymentId: String,
-    razorpayOrderId: String,
-    razorpaySignature: String,
+  ngoid: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "NGO",
+    required: true,
+  },
+  donorid: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Donor",
+    required: true,
+  },
+  amount: {
+    type: Number,
+    required: true,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+    required: true,
+  },
+  razorpayPaymentId: String,
+  razorpayOrderId: String,
+  razorpaySignature: String,
 });
-
+//added comment
+//added comment
+//added comment
+//added comment
+//added comment
+//added comment
+//added comment
 const Transection = new mongoose.model("Transection", transectionSchema);
 
 export default Transection;
